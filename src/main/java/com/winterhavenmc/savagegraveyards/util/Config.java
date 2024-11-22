406f6ccac13b5abd1a7c997cfabf36f44356058e
Tim Savage <tim.savage@gmail.com>
2024-11-22 08:21:19 -0600

    fix indent

--- src/main/java/com/winterhavenmc/savagegraveyards/util/Config.java
+++ src/main/java/com/winterhavenmc/savagegraveyards/util/Config.java
@@ -26,100 +26,118 @@ import java.util.Locale;
 
 public enum Config {
 
-    DEBUG(Boolean.FALSE),
-    LANGUAGE(Locale.US.toLanguageTag()),
-    ENABLED_WORLDS(List.of()),
-    DISABLED_WORLDS(List.of()),
-    DEFAULT_ENABLED(Boolean.TRUE),
-    DEFAULT_HIDDEN(Boolean.TRUE),
-    SAFETY_TIME(15),
-    DISCOVERY_RANGE(50),
-    DISCOVERY_INTERVAL(40),
-    LIST_PAGE_SIZE(5),
-    RESPAWN_PRIORITY(EventPriority.NORMAL),
-    TITLES_ENABLED(Boolean.TRUE),
-    SOUND_EFFECTS(Boolean.TRUE),
-    CONSIDER_BEDSPAWN(Boolean.FALSE);
+	DEBUG(Boolean.FALSE),
+	LANGUAGE(Locale.US.toLanguageTag()),
+	ENABLED_WORLDS(List.of()),
+	DISABLED_WORLDS(List.of()),
+	DEFAULT_ENABLED(Boolean.TRUE),
+	DEFAULT_HIDDEN(Boolean.TRUE),
+	SAFETY_TIME(15),
+	DISCOVERY_RANGE(50),
+	DISCOVERY_INTERVAL(40),
+	LIST_PAGE_SIZE(5),
+	RESPAWN_PRIORITY(EventPriority.NORMAL),
+	TITLES_ENABLED(Boolean.TRUE),
+	SOUND_EFFECTS(Boolean.TRUE),
+	CONSIDER_BEDSPAWN(Boolean.FALSE);
 
-    private final Object defaultObject;
+	private final Object defaultObject;
 
 
-    /**
-     * Constructor for Enum members
-     * @param defaultObject the object passed
-     */
-    Config(final Object defaultObject) {
-        this.defaultObject = defaultObject;
-    }
+	/**
+	 * Constructor for Enum members
+	 *
+	 * @param defaultObject the object passed
+	 */
+	Config(final Object defaultObject) {
+		this.defaultObject = defaultObject;
+	}
 
-    /**
-     * Get corresponding key for Enum member, formatted for style used in config.yml file
-     * @return {@code String} the key as formatted in config.yml file
-     */
-    public String asFileKey() {
-        return this.toLowerKebabCase();
-    }
+	/**
+	 * Get corresponding key for Enum member, formatted for style used in config.yml file
+	 *
+	 * @return {@code String} the key as formatted in config.yml file
+	 */
+	public String asFileKey() {
+		return this.toLowerKebabCase();
+	}
 
-    /**
-     * Convert Enum member name to lower kebab case
-     * @return {@code String} the Enum member name as lower kebab case
-     */
-    private String toLowerKebabCase() {
-        return this.name().toLowerCase().replace('_', '-');
-    }
+	/**
+	 * Convert Enum member name to lower kebab case
+	 *
+	 * @return {@code String} the Enum member name as lower kebab case
+	 */
+	private String toLowerKebabCase() {
+		return this.name().toLowerCase().replace('_', '-');
+	}
 
-    /**
-     * Get default value for key, matching exactly the corresponding string in the default config.yml file
-     * @return {@code Object} the default object for the corresponding key
-     */
-    public Object getDefaultObject() {
-        return this.defaultObject;
-    }
+	/**
+	 * Convert Enum member name to upper snake case (used for testing)
+	 *
+	 * @return {@code String} the Enum member name converted to upper snake case
+	 */
+	public String toUpperSnakeCase() {
+		return this.name().toUpperCase().replace('-', '_');
+	}
 
-    /**
-     * Get value as boolean for corresponding key in current configuration
-     * @param configuration {@code Configuration} reference to the plugin current configuration instance
-     * @return {@code boolean} the referenced value in the current configuration
-     */
-    public boolean getBoolean(final Configuration configuration) {
-        return configuration.getBoolean(asFileKey());
-    }
+	/**
+	 * Get default value for key, matching exactly the corresponding string in the default config.yml file
+	 *
+	 * @return {@code Object} the default object for the corresponding key
+	 */
+	public Object getDefaultObject() {
+		return this.defaultObject;
+	}
 
-    /**
-     * Get value as int for corresponding key in current configuration
-     * @param configuration {@code Configuration} reference to the plugin current configuration instance
-     * @return {@code int} the referenced value in the current configuration
-     */
-    public int getInt(final Configuration configuration) {
-        return configuration.getInt(asFileKey());
-    }
+	/**
+	 * Get value as boolean for corresponding key in current configuration
+	 *
+	 * @param configuration {@code Configuration} reference to the plugin current configuration instance
+	 * @return {@code boolean} the referenced value in the current configuration
+	 */
+	public boolean getBoolean(final Configuration configuration) {
+		return configuration.getBoolean(asFileKey());
+	}
 
-    /**
-     * Get value as int for corresponding key in current configuration
-     * @param configuration {@code Configuration} reference to the plugin current configuration instance
-     * @return {@code long} the referenced value in the current configuration
-     */
-    public long getLong(final Configuration configuration) {
-        return configuration.getLong(asFileKey());
-    }
+	/**
+	 * Get value as int for corresponding key in current configuration
+	 *
+	 * @param configuration {@code Configuration} reference to the plugin current configuration instance
+	 * @return {@code int} the referenced value in the current configuration
+	 */
+	public int getInt(final Configuration configuration) {
+		return configuration.getInt(asFileKey());
+	}
 
-    /**
-     * Get value as String for corresponding key in current configuration
-     * @param configuration {@code Configuration} reference to the plugin current configuration instance
-     * @return {@code String} the referenced value in the current configuration
-     */
-    public String getString(final Configuration configuration) {
-        return configuration.getString(asFileKey());
-    }
+	/**
+	 * Get value as int for corresponding key in current configuration
+	 *
+	 * @param configuration {@code Configuration} reference to the plugin current configuration instance
+	 * @return {@code long} the referenced value in the current configuration
+	 */
+	public long getLong(final Configuration configuration) {
+		return configuration.getLong(asFileKey());
+	}
 
-    /**
-     * Get value as List of String for corresponding key in current configuration
-     * @param configuration {@code Configuration} reference to the plugin current configuration instance
-     * @return {@code List<String>} the referenced value in the current configuration
-     */
-    @SuppressWarnings("unused")
-    public List<String> getStringList(final Configuration configuration) {
-        return configuration.getStringList(asFileKey());
-    }
+	/**
+	 * Get value as String for corresponding key in current configuration
+	 *
+	 * @param configuration {@code Configuration} reference to the plugin current configuration instance
+	 * @return {@code String} the referenced value in the current configuration
+	 */
+	public String getString(final Configuration configuration) {
+		return configuration.getString(asFileKey());
+	}
+
+	/**
+	 * Get value as List of String for corresponding key in current configuration
+	 *
+	 * @param configuration {@code Configuration} reference to the plugin current configuration instance
+	 * @return {@code List<String>} the referenced value in the current configuration
+	 */
+	@SuppressWarnings("unused")
+	public List<String> getStringList(final Configuration configuration) {
+		return configuration.getStringList(asFileKey());
+	}
 
 }
