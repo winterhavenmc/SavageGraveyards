ae64f6a03273d50a16e74dcbb61531712813500b
Tim Savage <tim.savage@gmail.com>
2025-04-28 06:19:25 -0500

    rename import

--- src/main/java/com/winterhavenmc/savagegraveyards/util/SafetyManager.java
+++ src/main/java/com/winterhavenmc/savagegraveyards/util/SafetyManager.java
@@ -29,7 +29,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static com.winterhavenmc.util.messagebuilder.MessageBuilder.TICKS;
+import static com.winterhavenmc.library.messagebuilder.MessageBuilder.TICKS;
 
 
 /**
