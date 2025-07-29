7418c9435b74742c74387b59f2807d1f45aea31f
Tim Savage <tim.savage@gmail.com>
2025-07-29 06:47:57 -0500

    edit messages

--- src/main/java/com/winterhavenmc/savagegraveyards/adapters/datastore/sqlite/SQLiteNotice.java
+++ src/main/java/com/winterhavenmc/savagegraveyards/adapters/datastore/sqlite/SQLiteNotice.java
@@ -23,24 +23,24 @@ import com.winterhavenmc.savagegraveyards.plugin.util.Notice;
 public enum SQLiteNotice implements Notice
 {
 	ALREADY_INITIALIZED("SQLite datastore already initialized."),
-	SCHEMA_VERSION_NOT_FOUND("Could not read schema version!"),
+	SCHEMA_VERSION_NOT_FOUND("Could not read schema version."),
 	TABLE_NOT_FOUND("An error occurred while trying to check the existence of a table."),
 	SCHEMA_UPDATE_FAILED("An error occurred while trying to update the SQLite datastore schema."),
 	SCHEMA_UPDATE_V1_FAILED("An error occurred while trying to update the datastore to schema v1."),
 	DATABASE_CLOSE_SUCCESS("The SQLite datastore connection was successfully closed."),
 	DATABASE_CLOSE_FAILED("An error occurred while closing the SQLite datastore."),
-	GRAVEYARD_RECORD_NOT_FOUND("An error occurred while trying to select a Valid record from the SQLite database."),
+	GRAVEYARD_RECORD_NOT_FOUND("An error occurred while trying to select a graveyard record from the SQLite database."),
 	SELECT_NEAREST_GRAVEYARD_FAILED("An error occurred while trying to fetch the nearest graveyard record from the SQLite datastore."),
 	SELECT_NEAREST_GRAVEYARDS_FAILED("An error occurred while trying to fetch the nearest graveyard records from the SQLite datastore."),
-	SELECT_MATCHING_GRAVEYARD_KEYS_FAILED("An error occurred while trying to fetch matching Valid records from the SQLite datastore."),
-	SELECT_MATCHING_GRAVEYARD_NAMES_FAILED("An error occurred while trying to fetch matching Valid records from the SQLite datastore."),
+	SELECT_MATCHING_GRAVEYARD_KEYS_FAILED("An error occurred while trying to fetch matching graveyard records from the SQLite datastore."),
+	SELECT_MATCHING_GRAVEYARD_NAMES_FAILED("An error occurred while trying to fetch matching graveyard records from the SQLite datastore."),
 	SELECT_ALL_GRAVEYARDS_FAILED("An error occurred while trying to select all graveyard records from the SQLite datastore."),
-	SELECT_ALL_VALID_GRAVEYARDS_FAILED("An error occurred while trying to select all valid graveyard records from the SQLite datastore."),
-	SELECT_UNDISCOVERED_GRAVEYARD_RECORDS("An error occurred while trying to select undiscovered Valid records from the SQLite datastore."),
-	SELECT_UNDISCOVERED_GRAVEYARD_KEYS("An error occurred while trying to select undiscovered Valid keys from the SQLite datastore."),
+	SELECT_ALL_VALID_GRAVEYARDS_FAILED("An error occurred while trying to select all graveyard graveyard records from the SQLite datastore."),
+	SELECT_UNDISCOVERED_GRAVEYARD_RECORDS("An error occurred while trying to select undiscovered graveyard records from the SQLite datastore."),
+	SELECT_UNDISCOVERED_GRAVEYARD_KEYS("An error occurred while trying to select undiscovered graveyard keys from the SQLite datastore."),
 	INSERT_DISCOVERY_FAILED("An error occurred while trying to insert a discovery record into the SQLite datastore."),
-	UPDATE_GRAVEYARD_RECORD_FAILED("An error occurred while trying to update a Valid record into the SQLite datastore."),
-	DELETE_GRAVEYARD_RECORD_FAILED("An error occurred while attempting to delete a Valid record from the SQLite datastore."),
+	UPDATE_GRAVEYARD_RECORD_FAILED("An error occurred while trying to update a graveyard record into the SQLite datastore."),
+	DELETE_GRAVEYARD_RECORD_FAILED("An error occurred while attempting to delete a graveyard record from the SQLite datastore."),
 	;
 
 	private final String message;
