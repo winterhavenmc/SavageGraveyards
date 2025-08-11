b2f096d324dc7e42374fd33296acac4706c170f7
Tim Savage <tim.savage@gmail.com>
2025-08-11 03:14:31 -0500

    refactor methods

--- src/main/java/com/winterhavenmc/savagegraveyards/plugin/commands/CommandManager.java
+++ src/main/java/com/winterhavenmc/savagegraveyards/plugin/commands/CommandManager.java
@@ -18,6 +18,7 @@
 package com.winterhavenmc.savagegraveyards.plugin.commands;
 
 import com.winterhavenmc.savagegraveyards.plugin.PluginMain;
+import com.winterhavenmc.savagegraveyards.plugin.util.Macro;
 import com.winterhavenmc.savagegraveyards.plugin.util.MessageId;
 import com.winterhavenmc.savagegraveyards.plugin.util.SoundId;
 
@@ -27,6 +28,7 @@ import org.bukkit.command.TabExecutor;
 
 import javax.annotation.Nonnull;
 import java.util.*;
+import java.util.function.Predicate;
 
 
 /**
@@ -34,10 +36,7 @@ import java.util.*;
  */
 public final class CommandManager implements TabExecutor
 {
-	// reference to main class
 	private final PluginMain plugin;
-
-	// map of subcommands
 	private final SubcommandRegistry subcommandRegistry = new SubcommandRegistry();
 
 
@@ -48,19 +47,9 @@ public final class CommandManager implements TabExecutor
 	 */
 	public CommandManager(final PluginMain plugin)
 	{
-		// set reference to main class
 		this.plugin = plugin;
-
-		// register this class as command executor
 		Objects.requireNonNull(plugin.getCommand("graveyard")).setExecutor(this);
-
-		// register subcommands
-		for (SubcommandType subcommandType : SubcommandType.values())
-		{
-			subcommandRegistry.register(subcommandType.create(plugin));
-		}
-
-		// register help command
+		Arrays.stream(SubcommandType.values()).forEach(type -> subcommandRegistry.register(type.create(plugin)));
 		subcommandRegistry.register(new HelpSubcommand(plugin, subcommandRegistry));
 	}
 
@@ -74,27 +63,18 @@ public final class CommandManager implements TabExecutor
 	                                  final @Nonnull String alias,
 	                                  final String[] args)
 	{
-		// if more than one argument, use tab completer of subcommand
+		// if more than one argument, use subcommand tab completer
 		if (args.length > 1)
 		{
-			// get subcommand from map
-			Optional<Subcommand> optionalSubcommand = subcommandRegistry.getSubcommand(args[0]);
+			Optional<Subcommand> subcommand = subcommandRegistry.getSubcommand(args[0]);
 
-			// if no subcommand returned from map, return empty list
-			if (optionalSubcommand.isEmpty())
-			{
-				return Collections.emptyList();
-			}
-
-			// unwrap optional subcommand
-			Subcommand subcommand = optionalSubcommand.get();
-
-			// return subcommand tab completer output
-			return subcommand.onTabComplete(sender, command, alias, args);
+			return (subcommand.isPresent())
+					? subcommand.get().onTabComplete(sender, command, alias, args)
+					: Collections.emptyList();
 		}
 
-		// return list of subcommands for which sender has permission
-		return matchingCommands(sender, args[0]);
+		// return list of matching subcommand names for which sender has permission
+		return matchingNames(sender, args[0]);
 	}
 
 
@@ -107,58 +87,71 @@ public final class CommandManager implements TabExecutor
 							 final @Nonnull String label,
 							 final String[] args)
 	{
-		// convert args array to list
 		List<String> argsList = new ArrayList<>(Arrays.asList(args));
-
-		String subcommandName;
-
-		// get subcommandName, remove from front of list
-		if (!argsList.isEmpty())
-		{
-			subcommandName = argsList.removeFirst();
-		}
-		// if no arguments, set subcommandName to help
-		else
-		{
-			subcommandName = "help";
-		}
-
-		subcommandRegistry.getSubcommand(subcommandName)
-				.or(() -> {
-						plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_INVALID_COMMAND).send();
-						plugin.soundConfig.playSound(sender, SoundId.COMMAND_INVALID);
-						return subcommandRegistry.getSubcommand("help");
-					})
-				.ifPresent(subcommand -> subcommand.onCommand(sender, argsList));
+		String subcommandName = getSubcommandNameOrDefault(argsList);
+		getSubcommandOrFallback(sender, subcommandName).ifPresent(subcommand -> subcommand.onCommand(sender, argsList));
 
 		return true;
 	}
 
 
+	private String getSubcommandNameOrDefault(List<String> argsList)
+	{
+		return (!argsList.isEmpty())
+				? argsList.removeFirst()
+				: "help";
+	}
+
+
+	private Optional<Subcommand> getSubcommandOrFallback(final CommandSender sender,
+	                                           final String subcommandName)
+	{
+		Optional<Subcommand> subcommand = subcommandRegistry.getSubcommand(subcommandName);
+
+		return (subcommand.isEmpty())
+				? notifyInvalidCommand(sender, subcommandName)
+				: subcommand;
+	}
+
+
+	private Optional<Subcommand> notifyInvalidCommand(final CommandSender sender, final String subcommandName)
+	{
+		plugin.soundConfig.playSound(sender, SoundId.COMMAND_INVALID);
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_INVALID_COMMAND)
+				.setMacro(Macro.COMMAND_NAME, subcommandName)
+				.send();
+
+		return subcommandRegistry.getSubcommand("help");
+	}
+
+
 	/**
-	 * Get matching list of subcommands for which sender has permission
+	 * Get matching list of subcommand names for which sender has permission
 	 *
 	 * @param sender the command sender
 	 * @param matchString the string prefix to match against command names
-	 * @return List of String - command names that match prefix and sender has permission
+	 * @return List of String - command names that match prefix and sender permission
 	 */
-	private List<String> matchingCommands(final CommandSender sender, final String matchString)
+	private List<String> matchingNames(final CommandSender sender, final String matchString)
 	{
-		List<String> returnList = new ArrayList<>();
+		return subcommandRegistry.getNames().stream()
+				.filter(hasPermission(sender))
+				.filter(matchesPrefix(matchString))
+				.toList();
+	}
 
-		for (String subcommandName : subcommandRegistry.getKeys())
-		{
-			Optional<Subcommand> subcommand = subcommandRegistry.getSubcommand(subcommandName);
-			if (subcommand.isPresent())
-			{
-				if (sender.hasPermission(subcommand.get().getPermissionNode())
-						&& subcommandName.startsWith(matchString.toLowerCase()))
-				{
-					returnList.add(subcommandName);
-				}
-			}
-		}
-		return returnList;
+
+	private Predicate<String> hasPermission(final CommandSender sender)
+	{
+		return subcommandName -> subcommandRegistry.getSubcommand(subcommandName)
+				.map(subcommand -> sender.hasPermission(subcommand.getPermissionNode()))
+				.orElse(false);
+	}
+
+
+	private Predicate<String> matchesPrefix(final String prefix)
+	{
+		return subcommandName -> subcommandName.startsWith(prefix.toLowerCase());
 	}
 
 }
