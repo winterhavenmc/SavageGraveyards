2e2c3b035691deb8cc00a13fb690e8b7a7ed8230
Tim Savage <tim.savage@gmail.com>
2025-10-18 22:07:35 -0500

    edit for style

--- core/src/main/java/com/winterhavenmc/savagegraveyards/core/context/DiscoveryCtx.java
+++ core/src/main/java/com/winterhavenmc/savagegraveyards/core/context/DiscoveryCtx.java
@@ -18,8 +18,10 @@
 package com.winterhavenmc.savagegraveyards.core.context;
 
 import com.winterhavenmc.library.messagebuilder.MessageBuilder;
+
 import com.winterhavenmc.savagegraveyards.core.ports.datastore.DiscoveryRepository;
 import com.winterhavenmc.savagegraveyards.core.ports.datastore.GraveyardRepository;
+
 import org.bukkit.plugin.Plugin;
 
 
