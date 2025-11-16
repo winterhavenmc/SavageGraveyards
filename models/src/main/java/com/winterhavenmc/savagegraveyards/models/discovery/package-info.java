7d3d7268ab0b5713bbe345f9639febc8fc8fb54e
Tim Savage <tim.savage@gmail.com>
2025-11-16 05:39:13 -0600

    initial commit

---
+++ models/src/main/java/com/winterhavenmc/savagegraveyards/models/discovery/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Represents a graveyard discovery as an algebraic data type, implemented using a sealed interface
+ * with permitted types of {@link com.winterhavenmc.savagegraveyards.models.discovery.ValidDiscovery}
+ * or {@link com.winterhavenmc.savagegraveyards.models.discovery.InvalidDiscovery}.
+ * <p>
+ * <img src="doc-files/Discovery_structure.svg" alt="Discovery Structure"/>
+ */
+package com.winterhavenmc.savagegraveyards.models.discovery;
